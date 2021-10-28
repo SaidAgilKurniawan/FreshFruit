9d2e82b75343cacbac03a4a0e0a9bbe0bdf6042c
SaidAgilKurniawan <92496571+SaidAgilKurniawan@users.noreply.github.com>
2021-10-28 09:06:05 +0700

    Add files via upload

---
+++ FreshFruit/Model/Seller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.Model
+{
+    internal class Seller
+    {
+        private string name;
+        private BucketController bucket;
+        public Seller(string name, BucketController bucket)
+        {
+            this.name = name;
+            this.bucket = bucket;
+        }
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+        public void addFruit(Fruit fruit)
+        {
+            this.bucket.addFruit(fruit);
+        }
+
+    }
+}
